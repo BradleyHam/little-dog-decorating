3a74a7379c68d0e16a4b63ff057cdf9e9bf10878
bradleyham <bradleyjamesham@gmail.com>
2024-04-27 13:27:14 +1200

    service section made responsive

--- app/HomeComponents/Services.tsx
+++ app/HomeComponents/Services.tsx
@@ -17,16 +17,16 @@ export default function Services() {
     return (
         <div className='bg-light-background'>
         <div className='services container mx-auto py-28 space-y-12'>
-            <div className='px-5 service flex flex-col'>
+            <div className='px-5 service flex flex-col lg:flex-row'>
                 <div className='lg:w-2/3 bg-gray-300 h-[300px] relative'>
                     <Image src='/images/mica9.jpeg'   layout='fill'
         objectFit='cover'  alt='something'/>
                 </div>
                <ServiceText heading={ServiceData.exteriorPainting.heading } bulletPoints={ServiceData.exteriorPainting.bulletPoints}/>
             </div>
-            <div className='service flex flex-col px-5'>
+            <div className='service flex flex-col lg:flex-row px-5'>
                
-                <div className='lg:w-2/3 bg-gray-300 h-[300px] relative'>
+                <div className='lg:w-2/3 bg-gray-300 h-[300px] relative lg:order-1'>
                         <Image src='/images/base-hostel.webp'   layout='fill'
                 objectFit='cover'  alt='something'/>
                 </div>

234f2b61636a40ee8193288cb5b7c68f6ba71f9b
bradleyham <bradleyjamesham@gmail.com>
2024-06-05 20:51:25 +1200

    desktop navbar made smaller

--- app/SiteComponents/Navbar.tsx
+++ app/SiteComponents/Navbar.tsx
@@ -40,13 +40,13 @@ export default function Navbar() {
     }, []);
 
     return (
-        <div className={`navbar w-screen top-0 right-0 left-0 fixed z-50 transition-colors duration-300 ${isScrolled ? 'bg-brand-blue' : 'bg-transparent'}`}>
-            <div className="container px-[20px] mx-auto flex flex-row justify-between items-center text-white">
+        <div className={`navbar w-screen top-0 right-0 left-0 fixed z-50 transition-colors duration-300 ${isScrolled ? 'bg-brand-blue' : 'bg-transparent'} py-4`}>
+            <div className="container px-[20px] mx-auto flex flex-row justify-end items-center text-white">
                 <div onClick={handleToggle} className="hamburger-container cursor-pointer lg:hidden z-30 top-4 left-4 pt-4 pr-4 pb-4">
                     {open ? <IoClose size={25} /> : <IoMenu size={25} />}
                 </div>
-                <div className="logo hidden lg:block translate-y-2 p-4 rounded-full">
-                    <Image src={'/images/ldd-logo.png'} alt="logo" width={100} height={60} />
+                <div className="logo hidden lg:block translate-y-2 p-4 rounded-full absolute left-[40px] translate-y-[20px]">
+                    <Image src={'/images/ldd-logo.png'} alt="logo" width={100} height={60} /> 
                 </div>
                 {(open || isAnimating) && (
                     <MobileNav open={open} onClose={handleAnimationEnd} handleToggle={handleToggle} />

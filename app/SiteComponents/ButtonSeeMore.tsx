7d61463bd34effb6efe5074c8bc2029a40359c3e
bradleyham <bradleyjamesham@gmail.com>
2024-07-25 15:00:30 +0700

    attempt to change favicon

--- app/SiteComponents/ButtonSeeMore.tsx
+++ app/SiteComponents/ButtonSeeMore.tsx
@@ -3,7 +3,7 @@ import { FaArrowRightLong } from "react-icons/fa6";
 export default function ButtonSeeMore() {
     return (
         <div className='see-more text-white font-poppins flex items-center space-x-2 mt-[40px]'>
-            <a href='#'>See Our Projects</a>
+            <span>See Our Projects</span>
             <FaArrowRightLong />
         </div>
     );
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,34 +1,38 @@
-import type { Metadata } from "next";
-import "./globals.css";
-import { Poppins } from "next/font/google";
-import ModalClientManager from "./SiteComponents/ModalClientManager";
+import { Metadata } from 'next';
+import './globals.css';
+import { Poppins } from 'next/font/google';
+import ModalClientManager from './SiteComponents/ModalClientManager';
 
 const poppins = Poppins({
   subsets: ['latin'],
   display: 'swap',
   variable: '--font-poppins',
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900']
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
 });
 
 export const metadata: Metadata = {
-  title: "Little Dog Decorating",
-  description: "Professional painters in Queenstown",
+  title: 'Little Dog Decorating',
+  description: 'Professional painters in Queenstown',
   icons: {
-    icon: '/images/favicon.png',
+    icon: '/favicon.png', // Path to your favicon
   },
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en" className={poppins.variable}>
+      <head>
+        <link rel="icon" href="/favicon.png" sizes="32x32" />
+        <link rel="icon" href="/favicon.png" sizes="16x16" />
+        <link rel="apple-touch-icon" href="/favicon.png" />
+        <meta name="msapplication-TileImage" content="/favicon.png" />
+      </head>
       <body>
-        <ModalClientManager>
-          {children}
-        </ModalClientManager>
+        <ModalClientManager>{children}</ModalClientManager>
       </body>
     </html>
   );

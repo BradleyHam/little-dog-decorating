c0da82195f8c63cdd67efe54139b55d0af409091
bradleyham <bradleyjamesham@gmail.com>
2024-07-31 14:14:21 +0700

    favicon working in devlopment

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,12 +9,14 @@ const poppins = Poppins({
   variable: '--font-poppins',
   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
 });
-
 export const metadata: Metadata = {
   title: 'Little Dog Decorating',
   description: 'Professional painters in Queenstown',
   icons: {
-    icon: '/favicon.png', // Path to your favicon
+    icon: [
+      { url: '/favicon.png', type: 'image/png' },
+      { url: '/favicon.ico', sizes: 'any' },
+    ],
   },
 };
 
@@ -25,12 +27,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={poppins.variable}>
-      <head>
-        <link rel="icon" href="/favicon.png" sizes="32x32" />
-        <link rel="icon" href="/favicon.png" sizes="16x16" />
-        <link rel="apple-touch-icon" href="/favicon.png" />
-        <meta name="msapplication-TileImage" content="/favicon.png" />
-      </head>
       <body>
         <ModalClientManager>{children}</ModalClientManager>
       </body>
